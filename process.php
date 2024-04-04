7df2b5d7549b753e2e8bbb3cab87e91cc684df16
Everett Hanke <forgetekbusiness@gmail.com>
2024-04-04 15:56:54 -0400

    all data are moving between form and reciept. Pushing this update so I can access on pc later

--- process.php
+++ process.php
@@ -1,15 +1,17 @@
-<<!doctype html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Cupcake Receipt</title>
 </head>
 <body>
 
 <?php
+$name = $_POST['name'];
+echo "<p>$name</p>";
 $flavors = $_POST['flavor'];
 foreach($flavors as $flavor)
 {
